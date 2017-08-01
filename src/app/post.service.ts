c088bd3e652074295fd626b3a46aa9076a5b7f1e
oomusou <oomusou@gmail.com>
2017-08-01 22:30:26 +0800

    使用 HttpClient

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class PostService {
+  private URI = 'https://jsonplaceholder.typicode.com';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
 
+  getPosts(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>(this.URI + `/posts`);
+  }
 }
