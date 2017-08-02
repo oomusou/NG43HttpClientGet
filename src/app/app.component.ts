ec4cdab2d91ce0e4379e0f996de82d4b4bb86bee
oomusou <oomusou@gmail.com>
2017-08-02 22:08:17 +0800

    使用 async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Post } from './post';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  posts: Observable<Post[]>;
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit(): void {
+    this.posts = this.postService.getPosts();
+  }
 }
